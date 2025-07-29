ce54ce866f876f687487accd43c941d720fc3a62
anjalisarawgi <anjali.sarawgi9@gmail.com>
2025-07-29 11:47:03 +0000

    changes final

--- src/trainer.py
+++ src/trainer.py
@@ -121,6 +121,7 @@ def main(args):
      
     # for trocr small handwritten
     # feature_extractor = ViTFeatureExtractor.from_pretrained("microsoft/trocr-small-handwritten")
+    
     if args.encoder == "swin" or args.encoder == "swin_from_scratch":
         print("using swin feature extractor")
         feature_extractor = AutoFeatureExtractor.from_pretrained("microsoft/ ")
