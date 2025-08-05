7ee25d43afbe344d928c076e80d5b1855e798871
anjalisarawgi <anjali.sarawgi9@gmail.com>
2025-08-05 13:22:48 +0000

    submission anonymized

--- src/utils/seed.py
+++ src/utils/seed.py
@@ -9,5 +9,5 @@ def set_all_seeds(seed= 42):
     os.environ["PYTHONHASHSEED"] = str(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.deterministic=True
     torch.backends.cudnn.benchmark = False
