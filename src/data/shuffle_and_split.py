99bc3926ce532f55c4b24f02af5fd6f5a74d1952
Anjali Sarawgi <41316743+anjalisarawgi@users.noreply.github.com>
2025-08-05 14:48:58 +0200

    Update shuffle_and_split.py

--- src/data/shuffle_and_split.py
+++ src/data/shuffle_and_split.py
@@ -15,12 +15,12 @@ def split_data(data, seed, train_frac=0.8, val_frac=0.1):
     random.seed(seed)
     random.shuffle(data)
     n = len(data)
-    n_train = int(n * train_frac)
-    n_val   = int(n * val_frac)
-    n_test  = n - n_train - n_val  
-    train_data = data[:n_train]
-    val_data   = data[n_train : n_train + n_val]
-    test_data  = data[n_train + n_val :]
+    n_train =int(n * train_frac)
+    n_val =int(n * val_frac)
+    n_test= n - n_train - n_val  
+    train_data=data[:n_train]
+    val_data= data[n_train : n_train + n_val]
+    test_data =data[n_train + n_val :]
 
     print("Total samples:", {n}, "Train samples:", len(train_data), "Validation:", len(val_data), "Test:", len(test_data))
     return train_data, val_data, test_data
@@ -28,7 +28,7 @@ def split_data(data, seed, train_frac=0.8, val_frac=0.1):
 def main():
     # output_dir = "data/oldNepali_fullset/labels_v4"
     data = load_labels("data/oldNepali_fullset/labels_v4/labels_full.json")
-    train, test, val = split_data(data, seed=41)
+    train,test,val= split_data(data,seed=41)
     save_labels(train,os.path.join("data/oldNepali_fullset/labels_v4", "labels_train.json"))
     save_labels(val,os.path.join("data/oldNepali_fullset/labels_v4", "labels_val.json"))
     save_labels(test, os.path.join("data/oldNepali_fullset/labels_v4", "labels_test.json"))
