34293e1e4f6aa188124dd3cae65e0aa67be0f9fd
Anjali Sarawgi <41316743+anjalisarawgi@users.noreply.github.com>
2025-08-05 14:21:03 +0200

    Update labels_without_spaces.py

--- src/data/labels_without_spaces.py
+++ src/data/labels_without_spaces.py
@@ -1,10 +1,9 @@
 import json
 import os
 
-input_path = "data/oldNepali_fullset_aug8/labels.json"
-output_path = "data/oldNepali_fullset_aug8/labels_no_space.json"
 
-with open(input_path, "r", encoding="utf-8") as f:
+# input
+with open("data/oldNepali_fullset_aug8/labels.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 data_no_space = []
@@ -12,8 +11,9 @@ for entry in data:
     new_text = entry["text"].replace(" ", "")
     data_no_space.append({"text": new_text, "image_path": entry["image_path"]})
 
-os.makedirs(os.path.dirname(output_path), exist_ok=True)
-with open(output_path, "w", encoding="utf-8") as f:
+# os.makedirs(os.path.dirname(output_path), exist_ok=True)\
+# ouput
+with open("data/oldNepali_fullset_aug8/labels_no_space.json", "w", encoding="utf-8") as f:
     json.dump(data_no_space, f, indent=2, ensure_ascii=False)
 
 print("complete")
