a2b369c264d06228974959f36001ee09576ffc12
Anjali Sarawgi <41316743+anjalisarawgi@users.noreply.github.com>
2025-08-05 14:15:18 +0200

    Update grayscale.py

--- src/data/grayscale.py
+++ src/data/grayscale.py
@@ -2,22 +2,23 @@ import os
 import json
 from PIL import Image
 
+# to greyscale nagari so that it aligns with old nepali data corpus - visually esp"
 base_dir    = "data/nagari/original/train"
-input_dir   = os.path.join(base_dir, "images")
-output_dir  = os.path.join(base_dir, "images_grayscaled")
+input_dir   = "data/nagari/original/train/images"
+output_dir  = "data/nagari/original/train/images_grayscaled"
 os.makedirs(output_dir, exist_ok=True)
 
-labels_path = os.path.join(base_dir, "labels.json")
-with open(labels_path, "r", encoding="utf-8") as f:
+with open("data/nagari/original/train/labels.json", "r", encoding="utf-8") as f:
     labels = json.load(f)
 
 grayscale_labels = []
 for item in labels:
     orig_rel = item["image_path"]
     fname    = os.path.basename(orig_rel)
-
+    
     img = Image.open(os.path.join(input_dir, fname)).convert("L") # convert to grayscale
     new_rel = os.path.join("images_grayscaled", fname)
+    
     img.save(os.path.join(base_dir, new_rel))
     new_item = item.copy()
     new_item["image_path"] = new_rel
@@ -25,6 +26,6 @@ for item in labels:
 
 out_labels = os.path.join(base_dir, "labels_grayscaled.json")
 with open(out_labels, "w", encoding="utf-8") as f:
-    json.dump(grayscale_labels, f, ensure_ascii=False, indent=2)
+    json.dump(grayscale_labels, f, ensure_ascii=False, indent=2) # ascii false for old nepai
 
 print("complete")
