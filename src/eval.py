7b588ed45a0e9cc5ab31d3ebc51d62e77a8787c1
Anjali Sarawgi <41316743+anjalisarawgi@users.noreply.github.com>
2025-08-05 14:55:18 +0200

    Update eval.py

--- src/eval.py
+++ src/eval.py
@@ -9,38 +9,32 @@ from transformers import VisionEncoderDecoderModel, TrOCRProcessor, AutoTokenize
 from torchmetrics.functional.text import char_error_rate
 import unicodedata
 
-MODEL_DIR = "models/trocr-large-handwritten-BERT-oldNepaliSynthetic_105k_vnoisy-byteBPE-500_finetuned_on_nagari_finetuned_on_oldNepali_fullset_aug8"
-TEST_LABELS_PATH = "data/oldNepali_fullset/labels_normalized_final/labels_test.json"
-OUTPUT_CSV = "results/trocr_base_bert_byteBPE/predictions.csv"
+# to change - model dir , output csv, test labels - everything it is run (according to need)
 
-MAX_LENGTH = 256
-NUM_BEAMS = 5
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-# CLEANUP = re.compile(r'[\u00AD\u200B\u200C\u200D]')
-# def clean_text(text: str) -> str:
-#     text = unicodedata.normalize("NFC", text)
-#     text = CLEANUP.sub("", text)
-#     return re.sub(r"\s+", "", text)
+CLEANUP = re.compile(r'[\u00AD\u200B\u200C\u200D]')
+def clean_text(text: str) -> str:
+    text = unicodedata.normalize("NFC", text)
+    text = CLEANUP.sub("", text)
+    return re.sub(r"\s+", "", text)
 
 # model and processor
-model = VisionEncoderDecoderModel.from_pretrained(MODEL_DIR).to(DEVICE)
-tokenizer = AutoTokenizer.from_pretrained(MODEL_DIR)
-processor = TrOCRProcessor(
-    image_processor=AutoImageProcessor.from_pretrained("microsoft/trocr-large-handwritten"),
-    tokenizer=tokenizer
-)
+model_dir = "models/trocr-large-handwritten-BERT-oldNepaliSynthetic_105k_vnoisy-byteBPE-500_finetuned_on_nagari_finetuned_on_oldNepali_fullset_aug8"
+model = VisionEncoderDecoderModel.from_pretrained(model_dir).to(DEVICE)
+tokenizer = AutoTokenizer.from_pretrained(model_dir)
+processor = TrOCRProcessor(image_processor=AutoImageProcessor.from_pretrained("microsoft/trocr-large-handwritten"),tokenizer=tokenizer)
 model.config.update({
     "decoder_start_token_id": tokenizer.cls_token_id,
     "pad_token_id": tokenizer.pad_token_id,
     "eos_token_id": tokenizer.eos_token_id,
-    "max_length": MAX_LENGTH,
-    "num_beams": NUM_BEAMS,
+    "max_length": 256,
+    "num_beams": 5,
     "early_stopping": True,
     "no_repeat_ngram_size": 0
 })
-
-with open(TEST_LABELS_PATH, "r", encoding="utf-8") as f:
+# change here
+with open("data/oldNepali_fullset/labels_normalized_final/labels_test.json", "r", encoding="utf-8") as f:
     test_data = json.load(f)
 
 def predict_text(image_path):
@@ -86,6 +80,6 @@ total_chars = df["gt_length"].sum()
 total_errs = df["weighted_errors"].sum()
 weighted_cer = total_errs/total_chars
 
-df.to_csv(OUTPUT_CSV, index=False)
-print(f"Results saved to{OUTPUT_CSV}")
-print(f"Overall length-weighted CER:   {weighted_cer:.4f}") 
+df.to_csv( "results/trocr_base_bert_byteBPE/predictions.csv", index=False) # change here
+print("results saved")
+print("Overall weighted CER:", weighted_cer:.4f) 
