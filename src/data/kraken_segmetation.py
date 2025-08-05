37cdf0f18e0cb1ad01faa5a791584d12a7e422f7
Anjali Sarawgi <41316743+anjalisarawgi@users.noreply.github.com>
2025-08-05 14:16:06 +0200

    Update kraken_segmetation.py

--- src/data/kraken_segmetation.py
+++ src/data/kraken_segmetation.py
@@ -1,20 +1,17 @@
 import os
 import subprocess
 
-BIN_DIR = "data/oldNepali/raw/images"
-SEG_OUT_DIR = "data/oldNepali/processed/jsons"
 
-os.makedirs(SEG_OUT_DIR, exist_ok=True)
+# os.makedirs("data/oldNepali/processed/jsons", exist_ok=True)
 
 def segment_image(image_path, json_path):
         subprocess.run(["kraken", "-i", image_path, json_path, "segment", "-bl"], check=True)
 
 def batch_segment():
-    for fname in os.listdir(BIN_DIR):
+    for fname in os.listdir("data/oldNepali/raw/images"):
         input_path = os.path.join(BIN_DIR, fname)
         json_name = os.path.splitext(fname)[0] + ".json"
         json_path = os.path.join(SEG_OUT_DIR, json_name)
-
         segment_image(input_path, json_path)
 
 if __name__ == "__main__":
