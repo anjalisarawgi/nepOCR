74f904e0222f2ea9306ad226bafb445295df1895
Anjali Sarawgi <41316743+anjalisarawgi@users.noreply.github.com>
2025-08-05 15:01:33 +0200

    Update set_seed.py

--- src/utils/set_seed.py
+++ src/utils/set_seed.py
@@ -1,13 +1,14 @@
-import os
-import random
+import random 
 import numpy as np
 import torch
 
-def set_seed(seed=42):
+
+def set_all_seeds(seed: int = 42):
     random.seed(seed)
     np.random.seed(seed)
-    os.environ["PYTHONHASHSEED"] = str(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # for multi-GPU setups
+
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
